14d1d93b8e4b26cc236c1aa0b6a1a06aa10c2dd5
Piotr Sobczyk <sobbombo@gmail.com>
2016-03-21 23:10:27 +0100

    some plots for blog post

--- wstepna_analiza_calosc.R
+++ wstepna_analiza_calosc.R
@@ -29,3 +29,44 @@ confirmed.terrorism %>% filter(country_txt=="Poland") %>%
 #terrorist attacks in Ukraine are mostly related to separatist attacks in the eastern part of the country
 confirmed.terrorism %>% filter(country_txt=="Ukraine") %>% 
 	select(iyear, imonth, iday, city, nkill, nkillter, nwound, property, propextent_txt, scite1) -> terrorism_ukraine
+
+library(ggplot2)
+library(ggthemes)
+confirmed.terrorism %>% filter(iyear>2005) %>% group_by(country_txt) %>%
+	summarise(total_kills=floor(sum(nkill, na.rm=T)),
+						region_txt=head(region_txt,1)) %>% arrange(desc(total_kills)) %>%
+	select(country_txt, total_kills, region_txt) %>% top_n(20, total_kills) -> total_killed_10y
+total_killed_10y$country_txt <- factor(total_killed_10y$country_txt, levels=rev(total_killed_10y$country_txt))
+
+ggplot(total_killed_10y, aes(x=country_txt, y=total_kills, fill=region_txt)) +
+	geom_bar(stat = "identity") + theme_tufte() +
+	theme(legend.position = c(0.8, 0.2),
+				legend.text = element_text(size=18),
+				legend.title = element_text(size=22),
+				plot.title = element_text(size=24)) +
+	xlab("") + ylab("Łączna liczba zabitych") +
+	guides(fill=guide_legend("Region")) +
+	coord_flip() + expand_limits(x = 0, y = 0) +
+	ggtitle("Kraje o największej liczbie zabitych w zamachach terrorystycznych w latach 2006-2015")
+
+
+confirmed.terrorism %>% filter(iyear>2005) %>% group_by(region_txt) %>%
+	summarise(total_kills=floor(sum(nkill, na.rm=T))) %>% arrange(desc(total_kills)) %>%
+	select(total_kills, region_txt) %>% top_n(20, total_kills) -> total_killed_10y_regions
+total_killed_10y_regions$region_txt <- factor(total_killed_10y_regions$region_txt, levels=rev(total_killed_10y_regions$region_txt))
+total_killed_10y_regions$continent <- factor(total_killed_10y_regions$region_txt, levels=rev(total_killed_10y_regions$region_txt))
+levels(total_killed_10y_regions$continent)=c("Australasia & Oceania", "America", "Asia", "Europe", "America", "Asia",
+																						 				  "America", "Europe", "Asia", "Africa", "Asia", 
+																						 "Middle East & North Africa")
+
+
+ggplot(total_killed_10y_regions, aes(x=region_txt, y=total_kills, fill=continent)) +
+	geom_bar(stat = "identity") + theme_tufte() +
+	theme(legend.position = c(0.8, 0.2),
+				legend.text = element_text(size=18),
+				legend.title = element_text(size=22),
+				plot.title = element_text(size=24)) +
+	xlab("") + ylab("Łączna liczba zabitych") +
+	guides(fill=guide_legend("Kontynent")) +
+	coord_flip() + expand_limits(x = 0, y = 0) +
+	ggtitle("Regiony o największej liczbie zabitych w zamachach terrorystycznych w latach 2006-2015")
